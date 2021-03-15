19b005c0522e8e6a2377435856d884ff9b625812
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2021-03-15 18:20:44 +0300

    ╰(▔∀▔)╯

--- src/reducers/toolPriceReducer.js
+++ src/reducers/toolPriceReducer.js
@@ -1,12 +1,9 @@
-
 const initialState =
     {
-        price1: 111,
-        price2: 1131,
+
     }
 
 export const dataPriceReducer = ( state = initialState, action) => {
-
     return state;
 }
 
--- src/store.js
+++ src/store.js
@@ -2,18 +2,12 @@ import { createStore } from 'redux';
 import reducers from "./reducers/reducers";
 
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__&&
+const store = createStore(reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__&&
     window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
-store.subscribe(() => {
-    //console.log('subscribe' ,store.getState())
-} )
-
-
-store.dispatch({type: 'ADD_TOOL', toolName: 'Компост' })
-store.dispatch({type: 'ADD_TOOL', toolName: 'Диспачер' })
-
+store.subscribe(() => {} )
 
 window.store = store;
 
